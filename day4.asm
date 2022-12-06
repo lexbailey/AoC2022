c820b411806ee6b7442f03a48f0c8e8bd57ccba8
Lex Bailey <danieljabailey@gmail.com>
2022-12-06 01:15:34 +0000

    day4 done

--- day4.asm
+++ day4.asm
@@ -16,6 +16,8 @@ iy_cache:
 
 result:
     db 0,0,0,0
+result2:
+    db 0,0,0,0
 
 
 range_a:
@@ -30,7 +32,6 @@ range_d:
 check_overlap:
     push bc
     ld bc, 4
-    
     call gte32le
     cp 1
     jp nz, check_overlap_no
@@ -52,6 +53,24 @@ check_overlap:
     pop bc
     ret
 check_overlap_no:
+    ld a, 0
+    pop bc
+    ret
+
+check_overlap2: ; ix is a, iy is c, d
+    push bc
+    ld bc, 4
+    call gte32le
+    cp 1
+    jp nz, check_overlap2_no
+    add iy, bc
+    call gt32le
+    cp 1
+    jp z, check_overlap2_no
+    ld a, 1
+    pop bc
+    ret
+check_overlap2_no:
     ld a, 0
     pop bc
     ret
@@ -103,6 +122,47 @@ output:
     ld iy,(iy_cache)
     ld ix, result
     call p1_result   
+    call intro_p2
+compute_part2:
+    ld hl, 0xa000
+loop2:
+    ld ix, range_a
+    call parse32le
+    inc hl
+    ld ix, range_b
+    call parse32le
+    inc hl
+    ld ix, range_c
+    call parse32le
+    inc hl
+    ld ix, range_d
+    call parse32le
+    inc hl
+    ; parsed four numbers, check overlaps
+    ld ix, range_a
+    ld iy, range_c
+    call check_overlap2
+    cp 1
+    jp z, yes_overlap2
+    ld ix, range_c
+    ld iy, range_a
+    call check_overlap2
+    cp 1
+    jp z, yes_overlap2
+    jp no_overlap2
+yes_overlap2:
+    ld ix, result2
+    ld iy, dec_one
+    call add32le
+no_overlap2:    
+    ld a, (hl)
+    cp 0
+    jp z, output2
+    jp loop2
+output2:
+    ld iy,(iy_cache)
+    ld ix, result2
+    call p2_result       
 end:
     jp end
 
