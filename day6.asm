bd671b01859f94a060f82f5e2bb8fd540e25f519
Lex Bailey <danieljabailey@gmail.com>
2022-12-08 13:26:31 +0000

    day 6 part 2 done

--- day6.asm
+++ day6.asm
@@ -20,6 +20,11 @@ result:
 buffer:
     db 0,0,0,0
 
+buffer14:
+    db 0,0,0,0,0,0,0,0,0,0
+buffer14_4:
+    db 0,0,0,0
+
 check_unique:
     push af
     push ix
@@ -57,6 +62,7 @@ check_unique_fail:
     pop af
     ld a, 0
     ret
+
 
 start:
     ld (iy_cache),iy
@@ -121,6 +127,88 @@ output:
     ld iy,(iy_cache)
     ld ix, result
     call p1_result   
+    call intro_p2
+    jp compute2
+
+
+check_unique_14:
+    push hl
+    push bc
+    ld hl, buffer14
+    ld b, 13
+check_outer:
+    ld c, (hl)
+    inc hl
+    call check_unique_14_part
+    cp 0
+    jp z, check_outer_fail
+    djnz check_outer
+    ld a, 1
+    pop bc
+    pop hl
+    ret
+check_outer_fail:
+    pop bc
+    pop hl
+    ld a, 0
+    ret
+
+check_unique_14_part:
+    push hl
+    push bc
+check_loop:
+    ld a, (hl)
+    inc hl
+    cp c
+    jp z, check_unique_14_fail
+    djnz check_loop
+    pop bc
+    pop hl
+    ld a, 1
+    ret
+check_unique_14_fail:
+    pop bc
+    pop hl
+    ld a, 0
+    ret
+    
+
+compute2:
+    ; fill the 14 item ring buffer
+    ld hl, 0xa000
+    ld de, buffer14
+    ld bc, 14
+    ldir
+
+compute2_next:
+    ld b, 14
+    ld de, buffer14
+compute2_loop:
+    call check_unique_14
+    cp 1
+    jp z, found_start
+    ld a, (hl)
+    ld (de), a
+    inc hl
+    inc de
+    djnz compute2_loop
+    jp compute2_next
+
+
+found_start:
+    ld bc, 0xa000
+    xor a
+    sbc hl, bc
+    ld ix, result
+    ld (ix), l
+    ld (ix+1), h
+output2:
+    ld iy,(iy_cache)
+    ld ix, result
+    call p2_result   
+    
+
+
 end:
     jp end
 
